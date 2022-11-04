4b555481107e5ac15263f5354515173b5e706a0e
DarkKnightBZN <77510230+DarkKnightBZN@users.noreply.github.com>
2022-11-04 16:26:08 -0600

    th elink was correct, but the html text was wrong

--- src/components/MyGitHub.jsx
+++ src/components/MyGitHub.jsx
@@ -1,7 +1,7 @@
 const MyGitHub = () => {
     return (
         <p>
-            Link to GitHub: <a href="https://github.com/DarkKnightBZN/ReactDemo.git"> https://github.com/dldf/reactdemo</a>
+            Link to GitHub: <a href="https://github.com/DarkKnightBZN/ReactDemo.git"> https://github.com/DarkKnightBZN/ReactDemo.git</a>
         </p>
     )
 }
